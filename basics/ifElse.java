cd14369ebc29f2029f548f84c415bf2e7ed665d4
Rahul Gupta <rahulcodesog7@gmail.com>
2026-06-02 22:19:56 +0530

    if else learnt in java

---
+++ basics/ifElse.java
@@ -0,0 +1,22 @@
+// Given marks of a student, print on the screen:
+
+// Grade A if marks >= 90
+// Grade B if marks >= 70
+// Grade C if marks >= 50
+// Grade D if marks >= 35
+// Fail, otherwise.
+
+import java.util.*;
+public class ifElse{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int marks;
+        System.out.println("enter the marks :");
+        marks=sc.nextInt();
+        if(marks >= 90) System.out.println("Grade A");
+        else if(marks >= 70) System.out.println("Grade B");
+        else if(marks >= 50) System.out.println("Grade C");
+        else if(marks >= 35) System.out.println("Grade D");
+        else System.out.println("Fail");
+    }
+}
