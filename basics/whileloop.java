ba5c61a8b570d96629a31fc97be8860eb10a14a4
Rahul Gupta <rahulcodesog7@gmail.com>
2026-06-02 23:07:45 +0530

    removed the unused code from the while loop

--- basics/whileloop.java
+++ basics/whileloop.java
@@ -1,7 +1,7 @@
-import java.util.*;
+ 
 public class whileloop{
     public static void main(String args[]){
-        Scanner sc= new Scanner(System.in);
+       
         int high=50,sum=0;
         while(high>0){
             sum+=high;
