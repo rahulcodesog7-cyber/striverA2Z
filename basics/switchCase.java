46e108cf8784dbfb16a340bf7bdd5cc106e5c368
Rahul Gupta <rahulcodesog7@gmail.com>
2026-06-02 22:41:56 +0530

    switch case in java

---
+++ basics/switchCase.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class switchCase{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int day=0;
+        System.out.println("enter the day between 1 to 7:");
+        day=sc.nextInt();
+        switch(day){
+            case 1: {
+                        System.out.println("monday");
+                        break;
+                    }
+            case 2: {
+                        System.out.println("tuesday");
+                        break;
+                    }
+            case 3: {
+                        System.out.println("wednesday");
+                        break;
+                    }
+            case 4: {
+                        System.out.println("thursday");
+                        break;
+                    }
+            case 5: {
+                        System.out.println("friday");
+                        break;
+                    }
+            case 6: {
+                        System.out.println("saturday");
+                        break;
+                    }
+            case 7: {
+                        System.out.println("sunday");
+                        break;
+                    }
+            default:  System.out.println("invalid input");
+        }
+    }
+}
