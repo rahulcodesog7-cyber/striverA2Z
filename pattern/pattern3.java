6ff3041d6afca66c2d1d6ebb2313d072dd099dba
Rahul Gupta <rahulcodesog7@gmail.com>
2026-06-03 22:33:45 +0530

    completed pattern 3&4

---
+++ pattern/pattern3.java
@@ -0,0 +1,22 @@
+//pattern3
+// 1
+// 12
+// 123
+// 1234
+// 12345
+import java.util.*;
+public class pattern3{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int row=0;
+        System.out.println("enter the number of rows:");
+        row=sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
---
+++ pattern/pattern4.java
@@ -0,0 +1,22 @@
+//pattern4
+// 1
+// 22
+// 333
+// 4444
+// 55555
+import java.util.*;
+public class pattern4{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int row=0;
+        System.out.println("enter the number of rows:");
+        row=sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
